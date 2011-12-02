3b32d0d766a0a38d7452e351f205913a25196502
Martin <martin@sysbits.se>
2011-12-02 08:07:37 +0100

    Rename and comments

--- core/oden.php
+++ core/oden.php
@@ -12,35 +12,47 @@ class Oden
   public function __construct()
   {
     self::$instance = &$this;
-
+    
+    //Load required core files
     require_once (CORE_DIR . 'configure.php');
     $this->config = new OD_Configure();
     require_once (APPLICATION_DIR . 'configs' . DS . 'config.php');
-    require_once (CORE_DIR . 'magic_methods.php');
+    require_once (CORE_DIR . 'magicmethods.php');
     require_once (CORE_DIR . 'libs' . DS . 'odm.php');    
     require_once (CORE_DIR . 'database.php');
     require_once (CORE_DIR . 'load.php');
     require_once (CORE_DIR . 'model.php');
     require_once (CORE_DIR . 'controller.php');
     require_once (CORE_DIR . 'router.php');
-    require_once (CORE_DIR . 'error_handler.php');
-
-    require_once (CORE_DIR . 'libs' . DS . 'cache.php');    
-    $this->db = new OD_Database($this->config->get("db_driver"));
-    new ODM();
+    require_once (CORE_DIR . 'errorhandler.php');
+    require_once (CORE_DIR . 'libs' . DS . 'cache.php'); 
     
+    //Init required core classes   
+    $this->db = new OD_Database($this->config->get("db_driver"));    
+    new ODM();    
     $this->router = new OD_Router();
+    
+    //Set custom error handler
     set_error_handler("error_handler");
-    $this->_loadAndRunController();
+    
+    //Start
+    $this->loadAndRunController();
 
   }
-
+  
+  /*
+   * Return instance of Oden 
+   * @return Oden
+   */
   public static function &get_instance()
   {
     return self::$instance;
   }
-
-  private function _loadAndRunController()
+  
+  /*
+   * Load controller
+   */
+  private function loadAndRunController()
   {
     if ($this->router->getController() != "")
       $controller = $this->router->getController();
