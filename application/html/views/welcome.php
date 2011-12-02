56a2a421a4a1f6ed4e314d03e9e95b192304f90e
Martin <martin@sysbits.se>
2011-12-02 07:54:21 +0100

    Added HTML5 Boilerplate

--- application/html/views/welcome.php
+++ application/html/views/welcome.php
@@ -1,26 +1,59 @@
-<!DOCTYPE html>
-<html >
 
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>Oden framework</title>
-  <link type="text/css" rel="stylesheet" href="/css/default.css" media="screen" />
+  <title></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
 
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/libs/modernizr-2.0.6.min.js"></script>
 </head>
 
 <body>
-<?
 
-//echo $this->element('debugbar', array('debugFiles' => $debugFiles, 10))
+  <div id="container">
+    <header>
+      <h1>Oden Framework</h1>
+    </header>
+    <div id="main" role="main">
+     <h3>Welcome!</h3>
+    </div>
+    <footer>
 
-?>
-<h1>Welcome to the Oden framework!</h1>
-<?=
+    </footer>
+  </div> <!--! end of #container -->
 
-date('Y-m-d h:s');
 
-?><br />
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
 
+
+  <!-- scripts concatenated and minified via ant build script-->
+  <script defer src="js/plugins.js"></script>
+  <script defer src="js/script.js"></script>
+  <!-- end scripts-->
+
+
+  <script> // Change UA-XXXXX-X to be your site's ID
+    window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
+    Modernizr.load({
+      load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+    });
+  </script>
+
+
+  <!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+  
 </body>
 </html>
